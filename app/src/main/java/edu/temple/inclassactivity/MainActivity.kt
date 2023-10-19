29169a1fad4d2251e47bc8e8cf5706c5e7e5d1e0
Kenny Miller <63763541+knnymllr@users.noreply.github.com>
2023-10-19 14:51:32 -0400

    support fragment manager, I see cake

--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -14,6 +14,8 @@ class MainActivity : AppCompatActivity() {
         typedArray.recycle()
 
         // Attach an instance of ImageDisplayFragment using factory method
-        ImageDisplayFragment.newInstance(imageArray)
+        var fragment = ImageDisplayFragment.newInstance(imageArray)
+
+        supportFragmentManager.beginTransaction().add(R.id.fragmentContainerView, fragment).commit()
     }
 }
